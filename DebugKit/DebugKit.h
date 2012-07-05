20b68e2273d1923df0aeaa692628016e8ad134c4
Chad Gibbons <dcgibbons@gmail.com>
2012-07-05 15:50:16 -0600

    add logging debug macros

--- DebugKit/DebugKit.h
+++ DebugKit/DebugKit.h
@@ -23,3 +23,18 @@
 #import "HexDump.h"
 #import "NSData+HexDump.h"
 #import "NSString+HexDump.h"
+
+//
+// Logging macros from http://stackoverflow.com/questions/969130/nslog-tips-and-tricks
+//
+#ifdef DEBUG
+#   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#   define DLog(...)
+#endif
+#define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#ifdef DEBUG
+#   define ULog(fmt, ...)  { UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%s\n [Line %d] ", __PRETTY_FUNCTION__, __LINE__] message:[NSString stringWithFormat:fmt, ##__VA_ARGS__]  delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil]; [alert show]; }
+#else
+#   define ULog(...)
+#endif
